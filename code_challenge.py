b5c91b076495d869418c2e6f8605b37b14bb2799
alone-ai7 <elijahtahadlangit7@gmail.com>
2026-08-16 03:49:14 -0700

    Add files via upload

---
+++ code_challenge.py
@@ -0,0 +1,15 @@
+
+#first_half
+mid_top = "\t\t\t\t\t\t\t\t\t\t*"
+side_mid = "\t\t\t\t\t\t\t\t\t*\t\t*"
+mid_mid = "\t\t\t\t\t\t\t\t*\t\t\t\t*"
+lower_up = "\t\t\t\t\t\t\t*\t\t  Hi! My name is\t\t*"
+lower_lower = "\t\t\t\t\t\t*\t\t\t      Elijah\t\t\t\t*"
+ 
+#second_half
+upper_up = "\t\t\t\t\t\t\t*\t\t\t\t\t\t*"
+lw_mid_mid = "\t\t\t\t\t\t\t\t*\t\t\t\t*"
+lw_side_mid = "\t\t\t\t\t\t\t\t\t*\t\t*"
+mid_bot = "\t\t\t\t\t\t\t\t\t\t*"
+
+print(mid_top, "\n", side_mid, "\n", mid_mid, "\n", lower_up, "\n", lower_lower, "\n", upper_up, "\n", lw_mid_mid, "\n", lw_side_mid, "\n", mid_bot)
