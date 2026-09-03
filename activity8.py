21564ead6ed66fdc1b5a78dca0ced18666896d1e
alone-ai7 <elijahtahadlangit7@gmail.com>
2026-09-03 12:29:06 +0800

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,13 @@
+hobbies = ""
+
+hob = input("What are your hobbies? ---> ")
+hobbies += hob + ", "
+
+hob = input("Anything else? ---> ")
+hobbies += hob + ", "
+
+hob = input("More? ---> ")
+hobbies += hob + ". "
+
+print("So my hobbies are" , hobbies)
+
