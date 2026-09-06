09a0d8c6c7812007f296809e9a492273de010c8f
alone-ai7 <elijahtahadlangit7@gmail.com>
2026-09-06 15:01:12 +0800

    Add variables of different types in activity3.py

---
+++ activity3.py
@@ -0,0 +1,18 @@
+#integer variable
+myAge = 18
+
+#float variable
+myWeight = 46.5
+
+#string variable
+myAddress = "sa puso mo lang ako nakatira"
+password = "eHdi_mAhal_kItA_xD"
+
+#boolean variable
+isMarried = False
+
+print(myAge) 
+print(myWeight)
+print(myAddress)
+print(password)
+print(isMarried)
