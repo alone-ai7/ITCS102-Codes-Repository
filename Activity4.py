a3ada91a4af29b595f21cd3248fc4731c62b1be6
alone-ai7 <elijahtahadlangit7@gmail.com>
2026-09-06 14:54:27 +0800

    Add mottos to Activity4.py

---
+++ Activity4.py
@@ -0,0 +1,7 @@
+motto_1 = "\t\tHanggat di pumuputok ang ugat, tuloy ang pagpupuyat"
+motto_2 = "\t\tInformation Technology is the new Meta"
+motto_3 = "\n\n\t\tIf it doesn't work, at least you're tired"
+
+print(motto_1)
+print(motto_2)
+print(motto_3)
