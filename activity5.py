47e5d6d14d3692414cee88e7ccb27023816f8d58
alone-ai7 <elijahtahadlangit7@gmail.com>
2026-09-06 15:21:16 +0800

    Create activity5.py

---
+++ activity5.py
@@ -0,0 +1,18 @@
+# + - addition
+# - - subtraction
+# * - multiplication
+# / - division
+# // - floor division
+# ** - exponentation
+# % - modulus
+
+a = int(input("Enter a number --> "))
+b = int(input("Enter a number --> "))
+
+print("The sum of a + b is ", a+b)
+print("The sum of a - b is ",a-b)
+print("The sum of a * b is ", a*b)
+print("The sum of a / b is ", a/b)
+print("The sum of a // b is ", a//b)
+print("The sum of a ** b is ", a**b)
+print("The sum of a % b is ", a%b)
