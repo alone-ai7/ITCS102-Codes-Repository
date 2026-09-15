5e9a28d4523d01daea9df39681a79d8cbd59fc2f
Elijah-Tahadlangit <elijahtahadlangit7@gmail.com>
2026-09-15 05:39:54 -0700

    Update activity14.py

--- activity14.py
+++ activity14.py
@@ -7,23 +7,25 @@ has_collateral = bool(input("Do you have collateral? (True/False): "))
 base_interest = 0.0
 
 if  age >= 21 and is_employed == True:
-    print("You are eligible for a loan.")
-    if credits >= 750:
+    pass
+    if credits >= 750:      #Tier one
         base_interest = 5.0
         if annual_income >= 100000:
             base_interest = 4.5
             print("Congratulations! You are approved for a loan of", base_interest, "%")
-    if 600 <= credits < 750:
-         base_interest = 8.0
-    elif has_collateral == True:
-              base_interest = 7.0
-              print("Congratulations! You are approved for a loan of", base_interest, "%")
-    else:
-        print("Rejected: Your credit score is too low.")
-    if annual_income < 40000:
-         base_interest = 9.5
-         print("Congratulations! You are approved for a loan of", base_interest, "%")
-    if credits < 600:
-         print("Rejected: Your credit score is too low.")      
 else:
-    print("Rejected: You are not eligible for a loan.")
+    print("Rejected: Fails baseline criteria.")
+ 
+if 600 <= credits < 750:                           #Tier two
+    base_interest = 8.0
+    
+    if annual_income < 40000:
+        risk_rate = 9.5
+        print("Congratulations! You are approved for a loan of", risk_rate, "%")
+    elif has_collateral == True:
+        base_interest = 7.0
+        print("Congratulations! You are approved for a loan of", base_interest, "%")
+          
+if credits < 600:                                                               #Tier three
+    print("Rejected: Your credit score is too low.")
+         
